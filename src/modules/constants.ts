e4b1cb7e9e998c6ea22b11b433b60b9321885f17
Adam Epling <adamepling@gmail.com>
2022-07-01 13:27:02 -0500

    Whoops! use the correct urls

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-export const WS_BASE_URL = 'ws://localhost:3003';
-export const BASE_URL = 'http://localhost:3003/';
+export const WS_BASE_URL = 'wss://chamber-api.herokuapp.com';
+export const BASE_URL = 'https://chamber-api.herokuapp.com/';
 
 export const ROUTES = [
   { title: 'Home', href: '/' },
