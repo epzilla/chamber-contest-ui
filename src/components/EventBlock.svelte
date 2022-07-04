e58a10dde9822db3487bc9509c10499000a12761
Adam Epling <adamepling@gmail.com>
2022-07-04 12:48:18 -0400

    Add links to contest in event blocks

--- src/components/EventBlock.svelte
+++ src/components/EventBlock.svelte
@@ -8,6 +8,19 @@
   export let isUpcomingEvent: boolean = false;
   $: didAttend = $userAttendedEvents.find(e => e.id === event.id);
   $: evType = event?.eventType[0];
+
+  function getUrlForContest() {
+    const now = new Date();
+    const start = new Date(event.startTime);
+    if (
+      now.getFullYear() === start.getFullYear() &&
+      now.getMonth() === start.getMonth()
+    ) {
+      return '/contest';
+    } else {
+      return `/contest/monthly/${start.getMonth() + 1}-${start.getFullYear()}`;
+    }
+  }
 </script>
 
 <li class="event" class:attended={didAttend}>
@@ -18,10 +31,13 @@
     </h3>
     <h5>
       {#if didAttend}
-        <span class="attendance-highlight"
-          >Your attendance earned you {evType.points}
-          {evType.points > 1 ? 'points' : 'point'}!</span
-        >
+        <a class="attendance-highlight" href={getUrlForContest()}
+          ><span>
+            Your attendance earned you {evType.points}
+            {evType.points > 1 ? 'points' : 'point'}!
+          </span>
+          <span class="fa fa-circle-arrow-right" />
+        </a>
       {:else}
         <span>{evType.type} - {evType.points}</span>
         <span>{evType.points > 1 ? 'points' : 'point'}</span>
@@ -71,8 +87,40 @@
       }
 
       h5 {
-        margin: 10px 0;
+        margin: 1rem 0;
         padding: 0 5px;
+      }
+    }
+  }
+
+  a.attendance-highlight {
+    padding: 8px;
+    color: white;
+  }
+
+  @media screen and (max-width: 600px) {
+    .event {
+      h5 {
+        width: 100%;
+      }
+      a.attendance-highlight {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr 1rem;
+        align-items: center;
+      }
+    }
+  }
+
+  @media screen and (min-width: 600px) {
+    .event {
+      a .attendance-highlight {
+        color: white;
+        display: inline;
+        padding: 0.5rem;
+        span:first-child {
+          margin-right: 10px;
+        }
       }
     }
   }
