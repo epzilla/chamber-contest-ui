f1d37092a6ca935e4d1db5c31fa648a6b614b84c
Adam Epling <adamepling@gmail.com>
2025-03-28 08:49:49 -0500

    Admin edit events improvements

--- src/routes/admin/edit-events/+page.svelte
+++ src/routes/admin/edit-events/+page.svelte
@@ -10,6 +10,16 @@
   let { events } = data;
   let isEditingEvent: ChamberEvent | null = null;
   let isAddingEvent: Partial<ChamberEvent> | null = null;
+  let search = '';
+
+  $: filteredEvents = search
+    ? events?.filter((e) => {
+        return (
+          e.title?.toLowerCase().includes(search.toLowerCase()) ||
+          e.address?.toLowerCase().includes(search.toLowerCase())
+        );
+      })
+    : events;
 
   function onAddSuccess(newEvent: ChamberEvent) {
     let allEvents = [...(events || []), newEvent];
@@ -54,31 +64,39 @@
 
 <section class="add-edit-events-page">
   <h3>Add/Edit Events</h3>
-  <button class="primary" style="margin-bottom: 10px;" on:click={onBeginAdd}
-    ><span class="fa fa-plus" />Add New Event</button
-  >
-  {#if events && !isEditingEvent}
+  <div class="top-btns-row">
+    <button class="primary" on:click={onBeginAdd}><span class="fa fa-plus" />Add New Event</button>
+    <!-- search bar -->
+    <div class="search-bar">
+      <span class="fa fa-search" />
+      <input type="search" class="search-bar" placeholder="Search..." bind:value={search} />
+    </div>
+  </div>
+  {#if filteredEvents && !isEditingEvent}
     <div class="events-table">
       <div class="table-header">
+        <div class="table-header-cell"></div>
         <div class="table-header-cell">Title</div>
-        <div class="table-header-cell large-screen">Address</div>
         <div class="table-header-cell">Date</div>
-        <div class="table-header-cell">Edit</div>
+        <div class="table-header-cell large-screen">Address</div>
       </div>
       <div class="table-body">
-        {#each events as event, i}
+        {#each filteredEvents as event, i}
           <div class={`table-row`}>
+            <div class="table-cell edit-cell">
+              <button class="edit-btn" on:click={() => (isEditingEvent = event)}>Edit</button>
+            </div>
             <div class="table-cell">
               {event.title}
             </div>
+            <div class="table-cell">
+              <DateTime
+                date={event.startTime || new Date().toISOString()}
+                dateFormat="MM/dd/yy, h:mm aaa"
+              />
+            </div>
             <div class="table-cell large-screen">
-              {event.address}
-            </div>
-            <div class="table-cell">
-              <DateTime date={event.startTime} dateFormat="MM/dd/yy, h:mm aaa" />
-            </div>
-            <div class="table-cell">
-              <button class="edit-btn" on:click={() => (isEditingEvent = event)}>Edit</button>
+              {event.address || ''}
             </div>
           </div>
         {/each}
@@ -123,21 +141,69 @@
     margin: 0 auto;
   }
 
+  .top-btns-row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+
+  .search-bar {
+    position: relative;
+    input {
+      padding: 0.5rem 0.5rem 0.5rem 2.25rem;
+      border: 1px solid var(--colorDisabledText);
+      border-radius: 5px;
+      width: 300px;
+    }
+
+    .fa {
+      position: absolute;
+      left: 0.8rem;
+      top: 0.66rem;
+      z-index: 1;
+      opacity: 0.5;
+    }
+  }
+
   .table-header,
   .table-row {
-    grid-template-columns: 4fr 4fr 2fr 1fr;
+    grid-template-columns: 70px 400px 180px auto;
     gap: 10px;
+
+    .table-header-cell,
+    .table-cell {
+      padding: 0 10px;
+    }
+
+    .table-header-cell {
+      padding: 10px;
+    }
+  }
+
+  .table-header {
+    background: #f0f0f0;
+    border-bottom: 1px solid var(--colorDisabledText);
   }
 
   .table-row {
     min-height: 40px;
     padding: 10px 0;
-    border-bottom: 1px solid var(--colorDefaultText);
+    border-bottom: 1px solid var(--colorDisabledText);
   }
 
   .edit-btn {
     height: 24px;
+    background-color: #5f3cb1;
+    color: white;
   }
+
+  .events-table {
+    background: white;
+    padding: 0;
+    border: 1px solid var(--colorDisabledText);
+  }
+
   @media screen and (max-width: 540px) {
     .table-header,
     .table-row {
